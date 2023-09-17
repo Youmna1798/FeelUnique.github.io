74a8c1175ba2e59c539bf16b4bd8fd2b49d1893c
Youmna1798 <yoma.samy98@gmail.com>
2023-09-17 11:37:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+    // var copyBtn = document.querySelectorAll("#copy-btn")
+    // // var copyPopup =document.querySelectorAll("#copy-popup")
+    // // function hover(){
+    // //     copyPopup.innerHTML="Press To Copy"
+    // //     copyPopup.style.display= "block"
+
+    // // }
+    // function press(){
+    //     // copyPopup.innerHTML="Copied"
+    //     alert("text copied")
+
+    // }
+    // copyBtn.addEventListener(onclick , press)
+    // // function hide(){
+    // //     copyPopup.innerHTML=""
+    // //     copyPopup.style.display= "none"
+
+
+    // // }
+
+    // // copyBtn.onmouseenter= hover
+    // // copyBtn.onclick = press
+    // // copyBtn.onmouseleave = hide
+
+
+
+
+    function coping(){
+        alert("Added to your bag")
+        
+    }
+
+ 
+      
+
+    function Send(){
+        alert("your feedback sended successfully")
+    }
